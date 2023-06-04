6d33a662fb8875d3d7803cdfac6f3a81a38c54a2
Domenico Pontari <fairsayan@gmail.com>
2023-06-04 14:44:25 +0200

    upgrade v20

--- build/index.d.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import ErrorManager from "./client";
-export default class ErrorManagerServer extends ErrorManager {
-    sender(res: express.Response, e?: unknown): void;
-    static get(e: unknown): import("./client").ErrorManagerResult;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from "express"
-import ErrorManager from "./client"
+import express from "express";
+import ErrorManager from "./client";
 
 export default class ErrorManagerServer extends ErrorManager {
-    sender(res:express.Response, e?:unknown) {
-        const err = this.get(e)
-        let statusCode = 500
+  sender(res: express.Response, e?: unknown) {
+    const err = this.get(e);
+    let statusCode = 500;
 
-        if (err.message.indexOf('not found') !== -1) statusCode = 404
-        res.status(statusCode).json(err)
-    }
+    if (err.message.indexOf("not found") !== -1) statusCode = 404;
+    res.status(statusCode).json(err);
+  }
 
-    static get(e:unknown) {
-        const errorManager = new ErrorManagerServer(e)
-        return errorManager.get()
-    }
+  static get(e: unknown) {
+    const errorManager = new ErrorManagerServer(e);
+    return errorManager.get();
+  }
 }
