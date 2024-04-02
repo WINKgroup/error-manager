826d06cfbbdb096613ef22c1bd24b21b3a63fed3
dom <fairsayan@gmail.com>
2024-04-02 14:09:23 +0200

    dependancies updated

--- src/client.ts
+++ src/client.ts
@@ -1,37 +1,37 @@
 export interface ErrorManagerResult {
-  status: "warn" | "error";
-  message: string;
+    status: 'warn' | 'error';
+    message: string;
 }
 
 export default class ErrorManager {
-  e?: unknown;
+    e?: unknown;
 
-  constructor(e?: unknown) {
-    this.e = e;
-  }
+    constructor(e?: unknown) {
+        this.e = e;
+    }
 
-  get(e?: unknown) {
-    if (!e) e = this.e;
-    console.error(e);
-    let error: ErrorManagerResult = {
-      status: "error",
-      message: "internal error",
-    };
+    get(e?: unknown) {
+        if (!e) e = this.e;
+        console.error(e);
+        let error: ErrorManagerResult = {
+            status: 'error',
+            message: 'internal error',
+        };
 
-    if (e instanceof Error) {
-      switch (e.name) {
-        case "MongoError":
-          if (e.message.indexOf("duplicate key") !== -1)
-            error.message = "duplication";
-          break;
-      }
-    } else if (typeof e === "string") error.message = e;
+        if (e instanceof Error) {
+            switch (e.name) {
+                case 'MongoError':
+                    if (e.message.indexOf('duplicate key') !== -1)
+                        error.message = 'duplication';
+                    break;
+            }
+        } else if (typeof e === 'string') error.message = e;
 
-    return error;
-  }
+        return error;
+    }
 
-  static get(e: unknown) {
-    const errorManager = new ErrorManager(e);
-    return errorManager.get();
-  }
+    static get(e: unknown) {
+        const errorManager = new ErrorManager(e);
+        return errorManager.get();
+    }
 }
